599b1e598471b164dc3ceec598946dcb9887be01
breitsch <breitsch@hotmail.com>
2020-04-02 21:48:22 +0200

    Initial commit

---
+++ src/ContaoHelloWorldBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of [package name].
+ *
+ * (c) Wr
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Wr\ContaoHelloWorldBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ContaoHelloWorldBundle extends Bundle
+{
+}
---
+++ tests/ContaoHelloWorldBundleTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of [package name].
+ *
+ * (c) John Doe
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Wr\ContaoHelloWorldBundle\Tests;
+use Wr\ContaoHelloWorldBundle\ContaoHelloWorldBundle;
+use PHPUnit\Framework\TestCase;
+
+class ContaoHelloWorldBundleTest extends TestCase
+{
+    public function testCanBeInstantiated()
+    {
+        $bundle = new ContaoHelloWorldBundle();
+
+        $this->assertInstanceOf('Wr\ContaoHelloWorldBundle\ContaoHelloWorldBundle', $bundle);
+    }
+}
